2c1ade82bc2c06611df17e0e84e770e1305163d1
Shumpei <shumpei.o.7g@gmail.com>
2023-11-23 05:39:03 +0900

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,110 @@
-import useRecorder from "./hooks/useRecorder";
+import { useCallback, useEffect, useState } from "react";
+import RecorderService from "./vendor/recoding-service/recording-service.js";
+
+type Recording = {
+  ts: number;
+  blobUrl: string;
+  mimeType: string;
+  size: number;
+};
 
 function App() {
-  const { audioURL, isRecording, startRecording, stopRecording } =
-    useRecorder();
+  const [recorderService, setRecorderService] =
+    useState<typeof RecorderService>();
+  const [recordings, setRecordings] = useState<Recording[]>([]);
+  const [recordingInProgress, setRecordingInProgress] = useState<boolean>();
+
+  const onNewRecording = useCallback(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (evt: any) => {
+      setRecordings([...recordings, evt.detail.recording]);
+    },
+    [recordings]
+  );
+
+  const handleRecording = useCallback(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (evt: any) => onNewRecording(evt),
+    [onNewRecording]
+  );
+
+  const startRecording = useCallback(async () => {
+    if (!recorderService) {
+      console.error("Recorder service is not initialized");
+      return;
+    }
+
+    recorderService.config.stopTracksAndCloseCtxWhenFinished = true;
+    recorderService.config.createDynamicsCompressorNode = false;
+    recorderService.config.enableEchoCancellation = true;
+
+    try {
+      await recorderService.startRecording(0);
+      setRecordingInProgress(true);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [recorderService, setRecordingInProgress]);
+
+  const stopRecording = useCallback(() => {
+    recorderService?.stopRecording();
+    setRecordingInProgress(false);
+  }, [recorderService, setRecordingInProgress]);
+
+  const handleClickRecording = useCallback(() => {
+    if (recordingInProgress) {
+      stopRecording();
+      recorderService?.em.removeEventListener("recording", handleRecording);
+    } else {
+      recorderService?.em.addEventListener("recording", handleRecording);
+      startRecording();
+    }
+  }, [
+    recorderService,
+    recordingInProgress,
+    handleRecording,
+    stopRecording,
+    startRecording,
+  ]);
+
+  const handleKeyPress = useCallback(() => {
+    window.removeEventListener("keypress", handleKeyPress);
+    handleClickRecording();
+  }, [handleClickRecording]);
+
+  useEffect(() => {
+    if (recorderService) {
+      window.addEventListener("keypress", handleKeyPress);
+      return () => {
+        window.removeEventListener("keypress", handleKeyPress);
+      };
+    }
+    setRecorderService(new RecorderService());
+  }, [recorderService, handleKeyPress]);
+
+  useEffect(() => {
+    if (recordingInProgress) {
+      window.addEventListener("keypress", handleKeyPress);
+    }
+  }, [recordingInProgress, handleKeyPress]);
+
+  useEffect(() => {
+    if (!recordings.length) {
+      return;
+    }
+    window.addEventListener("keypress", handleKeyPress);
+  }, [recordings, handleKeyPress]);
 
   return (
     <main>
-      <div>
-        <button onClick={startRecording} disabled={isRecording}>
-          Start Recording
-        </button>
-        <button onClick={stopRecording} disabled={!isRecording}>
-          Stop Recording
-        </button>
-        {audioURL && <audio src={audioURL} controls />}
-      </div>
+      <ol>
+        {recordings.map((recording) => (
+          <li key={recording.ts}>
+            <audio controls src={recording.blobUrl} />
+          </li>
+        ))}
+      </ol>
+      <button onClick={handleClickRecording}>録音する</button>
     </main>
   );
 }
---
+++ src/vendor/recoding-service/recording-service.d.ts
@@ -0,0 +1,36 @@
+export declare module "./recording-service.js" {
+  export interface IRecordingService {
+    baseUrl: string;
+    em: DocumentFragment;
+    state: string;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    chunks: any[];
+    chunkType: string;
+    encoderMimeType: string;
+    config: {
+      broadcastAudioProcessEvents: boolean;
+      createAnalyserNode: boolean;
+      createDynamicsCompressorNode: boolean;
+      forceScriptProcessor: boolean;
+      manualEncoderId: string;
+      micGain: number;
+      processorBufferSize: number;
+      stopTracksAndCloseCtxWhenFinished: boolean;
+      usingMediaRecorder: boolean;
+      enableEchoCancellation: boolean;
+      deviceId?: string;
+    };
+    // constructor(baseUrl: string): void;
+    // createWorker(fn: Function): Worker;
+    // startRecording(timeSlice: any): Promise<void> | undefined;
+    // setMicGain(newMicGain: any): void;
+    // _startRecordingWithStream(stream: any, timeSlice: any): void;
+    // _onAudioProcess(e: any): void;
+    // stopRecording(): void;
+    // _onDataAvailable(evt: any): void;
+    // _onError(evt: any): void;
+  }
+
+  const RecorderService = new RecordingService();
+  export default RecorderService;
+}
